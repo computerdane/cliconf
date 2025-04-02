bc49746c58f781d2420bbad14c3b7790aab792e0
dane <danerieber@gmail.com>
2025-04-01 20:55:41 -0500

    write some tests

--- src/lib.rs
+++ src/lib.rs
@@ -21,9 +21,7 @@ pub struct Flag<'a> {
     pub name: &'a str,
     pub shorthand: Option<char>,
     pub default_value: FlagValue,
-    pub env_var_delimeter: &'a str,
-    pub description: &'a str,
-    pub exclude_from_usage: bool,
+    pub description: Option<&'a str>,
 }
 
 pub struct Gears<'a> {
@@ -146,6 +144,7 @@ impl<'a> Gears<'a> {
             _ => panic!("Flag '{name}' is not of type f64!"),
         }
     }
+
     pub fn get_string_array(&self, name: &str) -> &Vec<String> {
         match self.flag_values.get(name) {
             Some(FlagValue::StringArrayValue(v)) => v,
@@ -183,13 +182,13 @@ impl<'a> Gears<'a> {
 
     fn parse_string_and_set(
         flag_values: &mut HashMap<&'a str, FlagValue>,
-        name: &String,
+        name: &str,
         value: String,
     ) -> Result<(), String> {
         let error_msg =
             |t: &str| format!("Failed to parse type {t} from flag '{name}' with value '{value}'");
 
-        let name = name.as_str();
+        let name = name;
 
         match flag_values.get(name) {
             Some(FlagValue::BoolValue(_)) => Gears::set(
@@ -264,6 +263,7 @@ impl<'a> Gears<'a> {
                 {
                     return Err(e);
                 }
+                need_value_for_name = String::from("");
             } else if arg == "-" {
                 self.positionals.push(arg)
             } else if arg == "--" {
@@ -322,6 +322,110 @@ impl<'a> Gears<'a> {
         // 3. Args
         self.parse_args(args)?;
 
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn init<'a>() -> Gears<'a> {
+        let mut gears = Gears::new();
+        gears.add(Flag {
+            name: "my-bool",
+            shorthand: Some('b'),
+            default_value: FlagValue::BoolValue(false),
+            description: None,
+        });
+        gears.add(Flag {
+            name: "my-string",
+            shorthand: Some('s'),
+            default_value: FlagValue::StringValue(String::from("1")),
+            description: None,
+        });
+        gears.add(Flag {
+            name: "my-int32",
+            shorthand: Some('i'),
+            default_value: FlagValue::Int32Value(1),
+            description: None,
+        });
+        gears.add(Flag {
+            name: "my-int64",
+            shorthand: Some('j'),
+            default_value: FlagValue::Int64Value(1),
+            description: None,
+        });
+        gears.add(Flag {
+            name: "my-float32",
+            shorthand: Some('f'),
+            default_value: FlagValue::Float32Value(1.0),
+            description: None,
+        });
+        gears.add(Flag {
+            name: "my-float64",
+            shorthand: Some('g'),
+            default_value: FlagValue::Float64Value(1.0),
+            description: None,
+        });
+        return gears;
+    }
+
+    fn assert_new_values(gears: &Gears) {
+        assert!(*gears.get_bool("my-bool") == true);
+        assert!(*gears.get_string("my-string") == "0");
+        assert!(*gears.get_i32("my-int32") == 0);
+        assert!(*gears.get_i64("my-int64") == 0);
+        assert!(*gears.get_f32("my-float32") == 0.0);
+        assert!(*gears.get_f64("my-float64") == 0.0);
+    }
+
+    fn to_string_vec(strs: Vec<&str>) -> Vec<String> {
+        strs.iter().map(|s| s.to_string()).collect()
+    }
+
+    #[test]
+    fn test_get() {
+        let gears = init();
+        assert!(*gears.get_bool("my-bool") == false);
+        assert!(*gears.get_string("my-string") == String::from("1"));
+        assert!(*gears.get_i32("my-int32") == 1);
+        assert!(*gears.get_i64("my-int64") == 1);
+        assert!(*gears.get_f32("my-float32") == 1.0);
+        assert!(*gears.get_f64("my-float64") == 1.0);
+    }
+
+    #[test]
+    fn test_parse_string_and_set() -> Result<(), String> {
+        let mut gears = init();
+        Gears::parse_string_and_set(&mut gears.flag_values, "my-bool", String::from("true"))?;
+        Gears::parse_string_and_set(&mut gears.flag_values, "my-string", String::from("0"))?;
+        Gears::parse_string_and_set(&mut gears.flag_values, "my-int32", String::from("0"))?;
+        Gears::parse_string_and_set(&mut gears.flag_values, "my-int64", String::from("0"))?;
+        Gears::parse_string_and_set(&mut gears.flag_values, "my-float32", String::from("0.0"))?;
+        Gears::parse_string_and_set(&mut gears.flag_values, "my-float64", String::from("0.0"))?;
+        assert_new_values(&gears);
+        Ok(())
+    }
+
+    #[test]
+    fn test_parse_args() -> Result<(), String> {
+        let mut gears = init();
+        gears.parse_args(to_string_vec(vec![
+            "cmd",
+            "--my-bool",
+            "--my-string",
+            "0",
+            "--my-int32",
+            "0",
+            "--my-int64",
+            "0",
+            "--my-float32",
+            "0.0",
+            "--my-float64",
+            "0.0",
+        ]))?;
+        assert_new_values(&gears);
         Ok(())
     }
 }
